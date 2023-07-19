d0f3a4b098cf2fa534d920538871600b39a77d22
pattan9955 <patricktan9955@hotmail.com>
2023-07-19 12:22:22 -0400

    Implement endpoints with async functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,4 +73,75 @@ public_users.get('/review/:isbn', function (req, res) {
     return res.status(200).send(JSON.stringify(reviews, null, 4));
 });
 
+// Async Portion (Tasks 10-13)
+public_users.get('/async-all-books', (req, res) => {
+    let promise = new Promise((resolve, reject) => {
+        resolve(books);
+    });
+    
+    promise
+        .then(value => {
+            return res.status(200).send(JSON.stringify(value, null, 4));
+        });
+});
+
+public_users.get('/async-isbn/:isbn', (req, res) => {
+    let promise = new Promise((resolve, reject) => {
+        let isbn = req.params.isbn;
+        
+        if (!(isbn in books)) {
+            reject("No such book exists");
+        }
+
+        resolve(books[isbn]);
+    });
+
+    promise
+        .then(book => {
+            return res.status(200).send(JSON.stringify(book, null, 4));
+        }, reason => {
+            return res.status(404).send(reason);
+        });
+});
+
+public_users.get('/async-author/:author', (req, res) => {
+    let promise = new Promise((resolve, reject) => {
+        let author = req.params.author;
+        let result = {"booksbyauthor": []};
+
+        for (const [isbn, book] of Object.entries(books)) {
+            if (book["author"] === author) {
+                result["booksbyauthor"].push({ [isbn]: book });
+            }
+        }
+
+        resolve(result);
+    });
+
+    promise
+        .then(result => {
+            return res.status(200).send(JSON.stringify(result, null, 4));
+        });
+});
+
+public_users.get('/async-title/:title', (req, res) => {
+    let promise = new Promise((resolve, reject) => {
+        let title = req.params.title;
+        let result = {"booksbytitle": []};
+
+        for (const [isbn, book] of Object.entries(books)) {
+            if (book["title"] === title) {
+                result["booksbytitle"].push({ [isbn]: book });
+            }
+        }
+
+        resolve(result);
+    });
+
+    promise
+        .then(result => {
+            return res.status(200).send(JSON.stringify(result, null, 4));
+        });
+});
+
 module.exports.general = public_users;
