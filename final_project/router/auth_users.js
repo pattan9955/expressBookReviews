a319707fd9318a5b556eea8da8020b855714f366
pattan9955 <patricktan9955@hotmail.com>
2023-07-18 22:54:41 -0400

    Implemented login and authorization

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,41 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+    let filteredUsers = users.filter((user) => user.username === username);
+    return (length(filteredUsers) > 0);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    for (let user of users) {
+        if (user.username === username && user.password === password) {
+            return true;
+        }
+    }
+    return false;
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    let [username, password] = [req.body.username, req.body.password];
+
+    if (!(username && password)) {
+        return res.status(401).send("Username or password not provided.");
+    }
+    if (!authenticatedUser(username, password)) {
+        return res.status(401).send("Invalid login. Check username and password.");
+    }
+
+    let accessToken = jwt.sign({ data: password }, "fingerprint_customer", { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken, username };
+
+    return res.status(200).send("Successfully logged in.")
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
